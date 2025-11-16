556d3de45b054c66d4eb36e06b047c28266af372
Dmitriy <dimakondykov@mail.ru>
2025-11-17 00:03:10 +0300

    Добавили функцию для работы с датой

--- src/masks.py
+++ src/masks.py
@@ -1,16 +1,18 @@
 def get_mask_card_number(card_number: str) -> str | None:
     """принимает на вход номер карты в виде числа и возвращает маску номера по правилу"""
-    if len(card_number) != 16:
+
+    if len(card_number) != 16: # проверяет длину номера карты и возвращает None, если она не равна 16 символам
         return None
 
     groups = []
-    for i in range(0, len(card_number), 4):
+    for i in range(0, len(card_number), 4):   # делаем срез внутри цикла по 4 символа
         groups.append(card_number[i: i + 4])
 
-    return " ".join([groups[0], groups[1][:2] + "**", "****", groups[3]])
+    return " ".join([groups[0], groups[1][:2] + "**", "****", groups[3]]) # объединяем по пробелам и делаем маску карты
+
 
 
 def get_mask_account(account_number: str) -> str:
     """принимает на вход номер счета в виде числа и возвращает маску номера по правилу"""
 
-    return "**" + account_number[-4:]
+    return "**" + account_number[-4:] # возвращаем маску счёта и последние 4 цифры
---
+++ src/widget.py
@@ -0,0 +1,35 @@
+from mypy.typeops import separate_union_literals
+
+from masks import get_mask_card_number, get_mask_account
+
+
+def mask_account_card(raw_card_number: str) -> str:
+    """Обрабатывает информацию как о картах, так и о счетах"""
+
+    card_number = raw_card_number.split(" ")  # делим принимаемую строку по пробелам
+
+    count_card_number = len(card_number)
+    type_card = ' '.join(card_number[:count_card_number - 1])  # Получаем тип карты
+
+    if type_card.lower() == "счет":
+        mask = get_mask_account(card_number[-1]) # берём только маску номера счёта
+    else:
+        mask = get_mask_card_number(card_number[-1])  # берём только маску номера карты
+
+    return type_card + ' ' + mask
+
+
+
+def get_date(date:str) -> str:
+    """функция принимает строку с датой и возвращает с датой в формате 'ДД.ММ.ГГГГ' """
+
+    separated_date = date.split("T") # Делим строку по символу 'Т'
+    date_str = separated_date[0]
+    new_date_str = date_str[8:10] + "." + date_str[5:7] + "." + date_str[:4] # делаем срез даты в формате 'ДД.ММ.ГГГГ'
+
+
+
+    return new_date_str
+
+
+print(get_date("2024-03-11T02:26:18.671407"))
