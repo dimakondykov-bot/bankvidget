60fdb65b5507283c660510f3e1334318d5f89626
Dmitriy <dimakondykov@mail.ru>
2025-11-17 00:13:30 +0300

    добавили функцию для работы с датой

--- src/widget.py
+++ src/widget.py
@@ -12,24 +12,18 @@ def mask_account_card(raw_card_number: str) -> str:
     type_card = ' '.join(card_number[:count_card_number - 1])  # Получаем тип карты
 
     if type_card.lower() == "счет":
-        mask = get_mask_account(card_number[-1]) # берём только маску номера счёта
+        mask = get_mask_account(card_number[-1])  # берём только маску номера счёта
     else:
         mask = get_mask_card_number(card_number[-1])  # берём только маску номера карты
 
     return type_card + ' ' + mask
 
 
-
-def get_date(date:str) -> str:
+def get_date(date: str) -> str:
     """функция принимает строку с датой и возвращает с датой в формате 'ДД.ММ.ГГГГ' """
 
-    separated_date = date.split("T") # Делим строку по символу 'Т'
+    separated_date = date.split("T")  # Делим строку по символу 'Т'
     date_str = separated_date[0]
-    new_date_str = date_str[8:10] + "." + date_str[5:7] + "." + date_str[:4] # делаем срез даты в формате 'ДД.ММ.ГГГГ'
-
-
+    new_date_str = date_str[8:10] + "." + date_str[5:7] + "." + date_str[:4]  # делаем срез даты в формате 'ДД.ММ.ГГГГ'
 
     return new_date_str
-
-
-print(get_date("2024-03-11T02:26:18.671407"))
