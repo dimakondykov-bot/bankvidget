4292f125072cd89c2b8a2c29079209a292b3064f
Dmitriy <dimakondykov@mail.ru>
2025-12-24 13:10:03 +0300

    ЛЛогирование кода. Добавил логи

--- src/masks.py
+++ src/masks.py
@@ -11,14 +11,19 @@ logger.addHandler(file_handler)
 def get_mask_card_number(card_number: str) -> str | None:
     """принимает на вход номер карты в виде числа и возвращает маску номера по правилу"""
 
-    if len(card_number) != 16:
-        logger.info(
-            'проверяем длину номера карты')  # проверяет длину номера карты и возвращает None, если она не равна 16 символам
+    if len(card_number) != 16: # проверяет длину номера карты и возвращает None, если она не равна 16 символам
+        logger.info('проверяем длину номера карты')
+
+        logger.error(f'ошибка длины номера карты: {len(card_number)}')
+        return None
+
+    if not card_number.isdigit(): # проверка формата карты, если содержит не только цифры
+        logger.error(f'ошибка формата карты: {card_number}')
         return None
 
     groups = []
-    for i in range(0, len(card_number), 4):
-        logger.info('делаем срез по 4 символа')  # делаем срез внутри цикла по 4 символа
+    for i in range(0, len(card_number), 4): # делаем срез внутри цикла по 4 символа
+        logger.info('делаем срез по 4 символа')
         groups.append(card_number[i: i + 4])
 
     # объединяем по пробелам и делаем маску карты
@@ -26,7 +31,13 @@ def get_mask_card_number(card_number: str) -> str | None:
     return " ".join([groups[0], groups[1][:2] + "**", "****", groups[3]])
 
 
-def get_mask_account(account_number: str) -> str:
+def get_mask_account(account_number: str) -> str | None:
     """принимает на вход номер счета в виде числа и возвращает маску номера по правилу"""
+
+    if not account_number.isdigit():
+        logger.error(f'ошибка формата номера счёта: {account_number}')
+        return None
+
+    mask = "**" + account_number[-4:]  # возвращаем маску счёта и последние 4 цифры
     logger.info('возвращает маску счёта по правилу')
-    return "**" + account_number[-4:]  # возвращаем маску счёта и последние 4 цифры
+    return mask
